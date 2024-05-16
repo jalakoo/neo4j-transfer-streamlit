e0dac6a4e524ca11af8353093ebc782f4b3dfcc5
jalakoo <jalakoo@gmail.com>
2024-05-16 00:26:34 -0700

    Session state added to handle incorrect source credentials and retry

--- neo4j_transfer_streamlit/main.py
+++ neo4j_transfer_streamlit/main.py
@@ -24,10 +24,37 @@ s_credentials = False
 t_credentials = False
 
 TRANSFER_LOG_KEY = "transfer_log"
+NODE_LABELS_KEY = "node_labels"
+RELATIONSHIP_TYPES_KEY = "relationship_types"
+SOURCE_URI_KEY = "source_uri"
+SOURCE_USER_KEY = "source_user"
+SOURCE_PASSWORD_KEY = "source_password"
+SOURCE_DATABASE_KEY = "source_database"
 
 if TRANSFER_LOG_KEY not in st.session_state:
     # Store a list of dictionaries containing transfer data
     st.session_state[TRANSFER_LOG_KEY] = []
+if NODE_LABELS_KEY not in st.session_state:
+    st.session_state[NODE_LABELS_KEY] = None
+if RELATIONSHIP_TYPES_KEY not in st.session_state:
+    st.session_state[RELATIONSHIP_TYPES_KEY] = None
+
+node_labels = None
+rel_types = None
+
+# Optionally load source database info from .env
+if SOURCE_URI_KEY not in st.session_state:
+    d_s_uri = os.environ.get("NEO4J_URI", None)
+    st.session_state[SOURCE_URI_KEY] = d_s_uri
+if SOURCE_USER_KEY not in st.session_state:
+    d_s_user = os.environ.get("NEO4J_USERNAME", "neo4j")
+    st.session_state[SOURCE_USER_KEY] = d_s_user
+if SOURCE_PASSWORD_KEY not in st.session_state:
+    d_s_password = os.environ.get("NEO4J_PASSWORD", None)
+    st.session_state[SOURCE_PASSWORD_KEY] = d_s_password
+if SOURCE_DATABASE_KEY not in st.session_state:
+    d_s_db = os.environ.get("NEO4J_DATABASE", "neo4j")
+    st.session_state[SOURCE_DATABASE_KEY] = d_s_db
 
 logger = logging.getLogger("neo4j_transfer")
 logger.setLevel(logging.DEBUG)
@@ -67,36 +94,59 @@ c1, c2, c3 = st.columns(3)
 with c1:
     st.write("Source Neo4j Database")
 
-    # Optionally load source database credentials from .env
-    d_s_uri = os.environ.get("NEO4J_URI", None)
-    d_s_user = os.environ.get("NEO4J_USERNAME", "neo4j")
-    d_s_password = os.environ.get("NEO4J_PASSWORD", None)
-    d_s_db = os.environ.get("NEO4J_DATABASE", "neo4j")
+    # with st.form("Source Neo4j Database"):
 
-    s_uri = st.text_input("URI", d_s_uri, key="s_uri")
-    s_user = st.text_input("Username", d_s_user, key="s_user")
+    s_uri = st.text_input("URI", st.session_state[SOURCE_URI_KEY], key="s_uri")
+    s_user = st.text_input("Username", st.session_state[SOURCE_USER_KEY], key="s_user")
     s_password = st.text_input(
-        "Password", d_s_password, key="s_password", type="password"
+        "Password",
+        st.session_state[SOURCE_PASSWORD_KEY],
+        key="s_password",
+        type="password",
     )
-    s_db = st.text_input("Database", d_s_db, key="s_db")
+    s_db = st.text_input("Database", st.session_state[SOURCE_DATABASE_KEY], key="s_db")
     if not bool(s_uri) or not bool(s_password):
         st.info(f"Enter source database info")
-        st.stop()
-    else:
-        s_creds = Neo4jCredentials(
-            uri=s_uri, username=s_user, password=s_password, database=s_db
-        )
-        s_nodes = nodes(s_creds)
-        s_relationships = relationships(s_creds)
+
+    # submitted = st.form_submit_button("Connect")
+    # if submitted:
+    if st.button("Connect"):
+        try:
+            s_creds = Neo4jCredentials(
+                uri=s_uri, username=s_user, password=s_password, database=s_db
+            )
+            validate_credentials(s_creds)
+            node_labels = nodes(s_creds)
+            rel_types = relationships(s_creds)
+            print(f"node_labels returned: {node_labels}")
+            print(f"rel_types returned: {rel_types}")
+            st.session_state[NODE_LABELS_KEY] = node_labels
+            st.session_state[RELATIONSHIP_TYPES_KEY] = rel_types
+            st.session_state[SOURCE_URI_KEY] = s_uri
+            st.session_state[SOURCE_USER_KEY] = s_user
+            st.session_state[SOURCE_PASSWORD_KEY] = s_password
+            st.session_state[SOURCE_DATABASE_KEY] = s_db
+            st.success("Connection successful")
+        except Exception as e:
+            st.error(f"Problem connecting with database: {e}")
+            st.stop()
+
 with c2:
-    st.write("Transfer Options")
     # Display source data options
-    if s_credentials == None:
-        st.info(f"Enter source database info")
+    node_options = st.session_state.get(NODE_LABELS_KEY, None)
+    relationship_options = st.session_state.get(RELATIONSHIP_TYPES_KEY, None)
+    if node_options is None or relationship_options is None:
         st.stop()
-    else:
-        nodes = st.multiselect("Nodes", options=s_nodes)
-        relationships = st.multiselect("Relationships", options=s_relationships)
+
+    # if node_labels is None or rel_types is None:
+    #     st.stop()
+
+    st.write("Transfer Options")
+
+    nodes = st.multiselect("Nodes", options=st.session_state[NODE_LABELS_KEY])
+    relationships = st.multiselect(
+        "Relationships", options=st.session_state[RELATIONSHIP_TYPES_KEY]
+    )
 
     if len(nodes) == 0 and len(relationships) == 0:
         st.info(f"Select nodes and/or relationships to transfer")
@@ -138,7 +188,13 @@ with c3:
                 st.warning("Select at least one node label to start a transfer")
             else:
                 try:
-                    result = transfer(s_creds, t_creds, spec)
+                    source_creds = Neo4jCredentials(
+                        uri=st.session_state[SOURCE_URI_KEY],
+                        username=st.session_state[SOURCE_USER_KEY],
+                        password=st.session_state[SOURCE_PASSWORD_KEY],
+                        database=st.session_state[SOURCE_DATABASE_KEY],
+                    )
+                    result = transfer(source_creds, t_creds, spec)
 
                     # Store list of transfer ids so an undo option is possible
                     st.session_state[TRANSFER_LOG_KEY].insert(
