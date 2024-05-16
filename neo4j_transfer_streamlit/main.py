086211de1a9af6072f44b7ec02f16073a526c092
jalakoo <jalakoo@gmail.com>
2024-05-15 23:03:55 -0700

    Undo option added

--- neo4j_transfer_streamlit/main.py
+++ neo4j_transfer_streamlit/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import logging
+import json
 
 from neo4j_transfer import (
     Neo4jCredentials,
@@ -8,15 +9,25 @@ from neo4j_transfer import (
     get_node_labels,
     get_relationship_types,
     validate_credentials,
+    undo,
 )
 import os
 
-st.set_page_config(layout="wide")
-c1, c2, c3 = st.columns(3)
+# Setup
+st.set_page_config(
+    page_title="Neo4j Transfer Tool", layout="wide", initial_sidebar_state="collapsed"
+)
+
 s_nodes = False
 s_relationships = False
 s_credentials = False
 t_credentials = False
+
+TRANSFER_LOG_KEY = "transfer_log"
+
+if TRANSFER_LOG_KEY not in st.session_state:
+    # Store a list of dictionaries containing transfer data
+    st.session_state[TRANSFER_LOG_KEY] = []
 
 logger = logging.getLogger("neo4j_transfer")
 logger.setLevel(logging.DEBUG)
@@ -51,6 +62,8 @@ def credentials_valid(creds) -> bool:
         st.error(f"Problem connecting with database with creds: {creds}: {e}")
 
 
+# Start UI
+c1, c2, c3 = st.columns(3)
 with c1:
     st.write("Source Neo4j Database")
 
@@ -72,7 +85,21 @@ with c1:
         )
         s_nodes = nodes(s_creds)
         s_relationships = relationships(s_creds)
+    else:
+        st.info(f"Enter source database info")
 with c2:
+    st.write("Transfer Options")
+    # Display source data options
+    if s_credentials == None:
+        st.info(f"Enter source database info")
+        st.stop()
+    else:
+        nodes = st.multiselect("Nodes", options=s_nodes)
+        relationships = st.multiselect("Relationships", options=s_relationships)
+
+    if len(nodes) == 0 and len(relationships) == 0:
+        st.info(f"Select nodes and/or relationships to transfer")
+with c3:
     st.write("Target Neo4j Database")
 
     # Optionally load target database credentials from .env
@@ -93,25 +120,46 @@ with c2:
         )
         credentials_valid(t_creds)
 
-with c3:
-    st.write("Transfer Options")
+        add_default_data = st.checkbox(
+            "Add default properties",
+            value=True,
+            help="Adds transfer detail properties to transferred nodes and relationships. Following key-values will be added: _transfer_element_id and _transfer_timestamp",
+        )
 
-    # Display source data options
-    if s_credentials == None:
-        st.info(f"Enter source database info")
-        st.stop()
+        spec = TransferSpec(
+            node_labels=nodes,
+            relationship_types=relationships,
+            should_append_data=add_default_data,
+        )
+
+        if st.button("Start Transfer"):
+            if len(nodes) == 0:
+                st.warning("Select at least one node label to start a transfer")
+            else:
+                try:
+                    result = transfer(s_creds, t_creds, spec)
+
+                    # Store list of transfer ids so an undo option is possible
+                    st.session_state[TRANSFER_LOG_KEY].insert(
+                        0, {"transfer_spec": spec.dict(), "result": result.dict()}
+                    )
+                    st.success(f"Transfer complete - {result}")
+                except Exception as e:
+                    st.error(f"Problem transferring: {e}")
+
     else:
-        nodes = st.multiselect("Nodes", options=s_nodes)
-        relationships = st.multiselect("Relationships", options=s_relationships)
-
-    # Present transfer button
-    if t_creds == False:
         st.info(f"Enter target database info")
-    else:
-        spec = TransferSpec(node_labels=nodes, relationship_types=relationships)
-        if st.button("Transfer"):
-            try:
-                result = transfer(s_creds, t_creds, spec)
-                st.success(f"Transfer complete - {result}")
-            except Exception as e:
-                st.error(f"Problem transferring: {e}")
+
+with st.sidebar:
+    st.header("Transfer Log")
+    logs = st.session_state[TRANSFER_LOG_KEY]
+    if len(logs) == 0:
+        st.write("<No prior transfers yet>")
+    for log in logs:
+        ts = log["transfer_spec"]["timestamp"]
+        with st.expander(f"{ts}"):
+            st.code(f"{log}")
+            if st.button("Undo"):
+                u_spec = TransferSpec(**log["transfer_spec"])
+                result = undo(t_creds, u_spec)
+                st.info(result.__dict__)
